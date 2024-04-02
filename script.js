f19910c0a2059d5c13bf6660977b832f1a5d6f57
Dymitr <dmitribaranowski51@gmail.com>
2024-04-02 13:24:57 +0300

    fox1

--- script.js
+++ script.js
@@ -98,7 +98,86 @@ let brand = "samSUnG";
 const isEqual = false == 0;
 // console.log(isEqual);
 
-/* Sweatch-----*/
+/* Switch-----*/
 
-const stars = 1;
-let price;
+// const stars = 8;
+// let price;
+
+// if (stars === 1) {
+//   price = 20;
+// } else if (stars === 2) {
+//   price = 30;
+// } else if (stars === 3) {
+//   price = 50;
+// } else if (stars === 4) {
+//   price = 70;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Такого количества звезд нет");
+// }
+
+// const stars = 1;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
+
+//   case 2:
+//     price = 30;
+//     break;
+//   case 3:
+//     price = 50;
+//     break;
+//   case 4:
+//     price = 70;
+//     break;
+//   case 5:
+//     price = 120;
+//     break;
+//   default:
+//     console.log("Такого количества звезд нет");
+// }
+// console.log(price);
+
+// const stars = 1;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//   price = 20;
+// } else if (stars === 2 || stars === 4) {
+//   price = 30;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Такого количества звезд нет");
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+//   default:
+//     console.log("Такого количества звезд нет");
+// }
+// console.log(price);
+
+// 1.Сделать переменную
+
+// сделать switch 123
+
+// 3.в каждом кейсе запись
