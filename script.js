400776d8ba973e8fb9a118b3cdcc65f662654bd6
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-04-09 21:01:13 +0300

    fox1

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
+// const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
 
-console.table(friends);
+// console.table(friends);
 
-friends[1] = "qweqwe";
-friends[3] = 123123;
+// friends[1] = "qweqwe";
+// friends[3] = 123123;
 
-console.table(friends);
+// console.table(friends);
 
 // const lastIndex = friends.length - 1;
 // console.log(lastIndex);
+
+// const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
+// console.table(friends);
+// const lastIndex = friends.length - 1;
+
+// for (let i = 0; i <= lastIndex; i += 1) {
+//   friends[i] += "-1";
+// }
+
+// for (const friend of friends) {
+//   console.log(friend);
+// }
+// console.table(friends);
+
+const cart = [54, 28, 105, 70, 92, 17, 120];
+
+let total = 0;
+
+// 1 перебрать масив
+
+for (let i = 0; i < cart.length; i += 1) {
+  console.log(cart[i]);
+
+  total += cart[i];
+}
+
+console.log("Total:", total);
+// 2 сделать переменную total до цикла
+
+// 2 каждый елемент приплюсовать к total
