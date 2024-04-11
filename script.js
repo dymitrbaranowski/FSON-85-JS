f829271a72f8b75c39e8ae3a6d477a74f582cb20
Dymitr <dmitribaranowski51@gmail.com>
2024-04-11 17:20:42 +0300

    fox1

--- script.js
+++ script.js
@@ -23,19 +23,48 @@
 // }
 // console.table(friends);
 
-const cart = [54, 28, 105, 70, 92, 17, 120];
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
 
-let total = 0;
+// let total = 0;
 
 // 1 перебрать масив
 
-for (let i = 0; i < cart.length; i += 1) {
-  console.log(cart[i]);
+// for (let i = 0; i < cart.length; i += 1) {
+//   console.log(cart[i]);
 
-  total += cart[i];
-}
+//   total += cart[i];
+// }
 
-console.log("Total:", total);
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log("Total:", total);
 // 2 сделать переменную total до цикла
 
 // 2 каждый елемент приплюсовать к total
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   cart[i] = Math.round(cart[i] * 1.1);
+// }
+
+// console.log(cart);
+
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+
+let total = 0;
+
+// 1 переменнная total
+// 2 перебрать масив
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+  // 3 на каждой итерации проверить елемент на четность
+
+  if (numbers[i] % 2 === 0) {
+    console.log("Четное!!!");
+    // 4 если четный, плюсуем к тотал
+    total += numbers[i];
+  }
+}
+
+console.log("Total:", total);
