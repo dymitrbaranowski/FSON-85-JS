a7e731f2ac7e69829e057e711fb24b962c56149b
Dymitr <dmitribaranowski51@gmail.com>
2024-04-17 15:23:25 +0300

    fox1

--- script.js
+++ script.js
@@ -151,12 +151,40 @@
 
 // console.log("bigestNumber:", bigestNumber);
 
-const friends = ["Mango", "POly", "Kiwi", "Ajax"];
+// const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
 
-let string = "";
+// let string = "";
 
-for (const friend of friends) {
-  string += friend + ",";
+// for (const friend of friends) {
+//   string += friend + ",";
+// }
+// string = string.slice(0, string.length - 1);
+
+// console.log(string);
+
+// const string = friends.join(",");
+// console.log(string);
+
+const string = "YJguwWdURYDFHllcd";
+const letters = string.split("");
+let invertedString = "";
+
+console.log(letters);
+
+for (const letter of letters) {
+  console.log(letter);
+
+  // if (letter === letter.toLowerCase()) {
+  //   console.log("Эта буква в нижнем регистре!!! - ", letter);
+
+  //   invertedString += letter.toUpperCase();
+  // } else {
+  //   console.log("Эта буква в верхнем регистре!!! - ", letter);
+  //   invertedString += letter.toLowerCase();
+  // }
+
+  const isEqual = letter === letter.toLowerCase();
+  invertedString += isEqual ? letter.toUpperCase() : letter.toLowerCase();
 }
 
-console.log(string.slice(string.length - 1));
+console.log("invertedString: ", invertedString);
