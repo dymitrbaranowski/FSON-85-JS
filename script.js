971aca4b980df3d5c0a626cb74f9872c7ba426d5
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-04-21 21:54:50 +0300

    fox1

--- script.js
+++ script.js
@@ -209,7 +209,10 @@ const array2 = [10, 20, 30];
 let total = 0;
 
 const numbers = array1.concat(array2);
-console.log(numbers);
 
-console.log(array1);
-console.log(array2);
+for (const number of numbers){
+  total += number
+}
+
+console.log(total);
+
