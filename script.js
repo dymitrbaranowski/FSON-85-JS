fbf522ca88d29c6984326e0aef682172334b718e
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-06-13 13:26:40 +0300

    fox1

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
-const users = [
-  {
-    name: "User1",
-    language: "html",
-    cars: {
-      audi: "a6",
-      bmw: "x5",
-    },
-  },
-  {
-    name: "User2",
-    language: "css",
-    cars: {
-      audi: "a8",
-      bmw: "x6",
-    },
-  },
-  {
-    name: "User3",
-    language: "js",
-    cars: {
-      audi: "a7",
-    },
-  },
-];
+// const users = [
+//   {
+//     name: "User1",
+//     language: "html",
+//     cars: {
+//       audi: "a6",
+//       bmw: "x5",
+//     },
+//   },
+//   {
+//     name: "User2",
+//     language: "css",
+//     cars: {
+//       audi: "a8",
+//       bmw: "x6",
+//     },
+//   },
+//   {
+//     name: "User3",
+//     language: "js",
+//     cars: {
+//       audi: "a7",
+//     },
+//   },
+// ];
+
+// // for (const {
+// //   name,
+// //   cars: { audi: car },
+// // } of users) {
+// //   console.log(name, car);
+// // }
 
 // for (const {
 //   name,
-//   cars: { audi: car },
+//   cars: { audi, bmw },
 // } of users) {
-//   console.log(name, car);
+//   console.log(name, bmw);
 // }
 
-for (const {
-  name,
-  cars: { audi, bmw },
-} of users) {
-  console.log(name, bmw);
-}
+// const products = [
+//   {
+//     name: "Radar",
+//     price: 1300,
+//     quantity: 9,
+//   },
+//   {
+//     name: "Scaner",
+//     price: 2700,
+//     quantity: 3,
+//   },
+//   {
+//     name: "Droid",
+//     price: 1100,
+//     quantity: 6,
+//   },
+//   {
+//     name: "Grip",
+//     price: 1450,
+//     quantity: 10,
+//   },
+// ];
+
+// function getAllPropValue(propName) {
+//   const newArr = [];
+
+//   for (const product of products) {
+//     propName in product ? newArr.push(product[propName]) : newArr;
+//   }
+//   return newArr;
+// }
+// console.log(getAllPropValue("price"));
