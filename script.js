e11c64586fdd74703c3041762c07be2f154d0051
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-07-19 14:27:08 +0300

    fox1

---
+++ script.js
@@ -0,0 +1,36 @@
+// const objA = {
+//   x: 5,
+//   showX() {
+//     console.log("this in objA.showX: ", this);
+//     console.log(this.x);
+
+//     const objB = {
+//       y: 10,
+//       showThis: () => {
+//         console.log("this in objB.showThis: ", this);
+//       },
+//     };
+
+//     objB.showThis();
+//   },
+// };
+
+// objA.showX();
+
+const numbers = [1, 5, 2, 4, 3];
+
+// const greaterThenTwo = numbers.filter(function (num) {
+//   return num > 2;
+// });
+
+const greaterThenTwo = numbers.filter((num) => num > 2);
+
+console.log(greaterThenTwo);
+
+const multyByTwo = greaterThenTwo.map((num) => num * 3);
+
+console.log(multyByTwo);
+
+const sorted = multyByTwo.sort((a, b) => a - b);
+
+console.log(sorted);
