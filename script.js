741992aee8f57979d1eaac2b82b3b73691741b08
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-07-22 07:58:40 -0700

    fox1

--- script.js
+++ script.js
@@ -43,7 +43,7 @@
 
 // console.table(updatedPlayers);
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
 // numbers.forEach(function (number) {
 //   console.log("number", number);
@@ -57,6 +57,28 @@ const numbers = [5, 10, 15, 20, 25];
 // console.log("numbers", numbers);
 // console.log("doubledNums", doubledNums);
 
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 410, points: 54, online: true },
+//   { id: "player-3", name: "Kiwi", timePlayed: 230, points: 54, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, points: 54, online: false },
+//   { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
+// ];
+
+// const playerNames = players.map((player) => player.name);
+// console.log("playerNames", playerNames);
+
+// const playerIds = players.map((player) => player.id);
+// console.log("playerIds", playerIds);
+
+// // const res = players.map(({ name, online }) => ({ name, online }));
+
+// // console.log("res", res);
+// const updatedPlayers = players.map((player) => {
+//   console.log(player);
+// });
+const numbers = [5, 10, 15, 20, 25];
+
 const players = [
   { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
   { id: "player-2", name: "Poly", timePlayed: 410, points: 54, online: true },
@@ -65,15 +87,11 @@ const players = [
   { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
 ];
 
-const playerNames = players.map((player) => player.name);
-console.log("playerNames", playerNames);
+const filteredNumbers = numbers.filter((number) => {
+  console.log(number);
 
-const playerIds = players.map((player) => player.id);
-console.log("playerIds", playerIds);
-
-// const res = players.map(({ name, online }) => ({ name, online }));
-
-// console.log("res", res);
-const updatedPlayers = players.map((player) => {
-  console.log(player);
+  return number < 15;
 });
+
+console.log(filteredNumbers);
+console.log("numbers", numbers);
