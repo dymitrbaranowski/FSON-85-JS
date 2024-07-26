5f5d7617c2b744060855a6896a970cc29e9a2db7
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-07-26 18:20:13 +0300

    fox1

--- script.js
+++ script.js
@@ -126,61 +126,42 @@
 // }, 0);
 // console.log(total);
 
-const salary = {
-  mango: 100,
-  poly: 50,
-  ajax: 150,
-};
+// const salary = {
+//   mango: 100,
+//   poly: 50,
+//   ajax: 150,
+// };
 
-const totalSalary = Object.values(salary).reduce(
-  (total, value) => total + value,
-  0
-);
+// const totalSalary = Object.values(salary).reduce(
+//   (total, value) => total + value,
+//   0
+// );
 
 // console.log(totalSalary);
 
-const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, points: 54, online: true },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 54, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, points: 54, online: false },
-  { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
-];
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 470, points: 54, online: true },
+//   { id: "player-3", name: "Kiwi", timePlayed: 230, points: 54, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, points: 54, online: false },
+//   { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
+// ];
 
-const totalTimePlayed = players.reduce(
-  (totalTime, { timePlayed }) => totalTime + timePlayed,
-  0
-);
+// const totalTimePlayed = players.reduce(
+//   (totalTime, { timePlayed }) => totalTime + timePlayed,
+//   0
+// );
 // console.log(totalTimePlayed);
 
-const cart = [
-  { lable: "Apples", price: 100, quantity: 2 },
-  { lable: "Bananas", price: 120, quantity: 3 },
-  { lable: "Lemons", price: 70, quantity: 4 },
-];
-const totalAmount = cart.reduce(
-  (total, { price, quantity }) => total + price * quantity,
-  0
-);
+// const cart = [
+//   { lable: "Apples", price: 100, quantity: 2 },
+//   { lable: "Bananas", price: 120, quantity: 3 },
+//   { lable: "Lemons", price: 70, quantity: 4 },
+// ];
+// const totalAmount = cart.reduce(
+//   (total, { price, quantity }) => total + price * quantity,
+//   0
+// );
 // console.log(totalAmount);
 
 // const getInactiveUsers = (array) => array.filter(({ isActive }) => !isActive);
-
-<<<<<<< Updated upstream
-const tweets = [
-  { id: "000", likes: 5, tags: ["js", "nodejs"] },
-  { id: "001", likes: 2, tags: ["html", "css"] },
-  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-  { id: "003", likes: 8, tags: ["css", "react"] },
-  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
-
-const allTags = tweets.reduce((acc, tweet) => [...acc, ...tweet.tags], []);
-console.log(allTags);
-=======
-// console.log("res", res);
-const updatedPlayers = players.map((player) => {
-  console.log(player);
-});
-
->>>>>>> Stashed changes
