f26bd6abc80fb4219722d3e74098509e8ac0fdbf
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-07-26 19:56:09 +0300

    fox4

--- script.js
+++ script.js
@@ -166,18 +166,30 @@ const players = [
 
 // const getInactiveUsers = (array) => array.filter(({ isActive }) => !isActive);
 
-const byName = [...players].sort((a, b) => {
-  console.log(a.name.charCodeAt(0));
-  const result = a.name[0] > b.name[0];
+// const byName = [...players].sort((a, b) => {
+//   console.log(a.name.charCodeAt(0));
+//   const result = a.name[0] > b.name[0];
 
-  if (result) {
-    return 1;
-  }
+//   if (result) {
+//     return 1;
+//   }
 
-  if (!result) {
-    return -1;
-  }
-});
-console.table(byName);
+//   if (!result) {
+//     return -1;
+//   }
+// });
+// console.table(byName);
 
-console.log("abc".charCodeAt(2));
+// console.log("abc".charCodeAt(2));
+
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+// const tags = tweets.map((t) => t.tags).flat();
+const tags = tweets.flatMap((t) => t.tags);
+console.log(tags);
