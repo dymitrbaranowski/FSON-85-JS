36289007c1bbe223d0aa47c6f292c4ef2d1857e1
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-08-08 19:51:18 +0300

    fox2

--- script.js
+++ script.js
@@ -139,13 +139,13 @@
 
 // console.log(totalSalary);
 
-const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, points: 54, online: true },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 54, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, points: 54, online: false },
-  { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
-];
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 470, points: 54, online: true },
+//   { id: "player-3", name: "Kiwi", timePlayed: 230, points: 54, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, points: 54, online: false },
+//   { id: "player-5", name: "Chelsy", timePlayed: 80, points: 54, online: true },
+// ];
 
 // const totalTimePlayed = players.reduce(
 //   (totalTime, { timePlayed }) => totalTime + timePlayed,
@@ -211,17 +211,17 @@ const players = [
 // const tags = tweets.flatMap((t) => t.tags);
 // console.log(tags);
 
-const numbers = [1, 5, 2, 4, 3];
+// const numbers = [1, 5, 2, 4, 3];
 
-const greaterThenTwo = numbers.filter(function (num) {
-  return num > 2;
-});
-console.log(greaterThenTwo);
+// const greaterThenTwo = numbers.filter(function (num) {
+//   return num > 2;
+// });
+// console.log(greaterThenTwo);
 
-const multByThree = greaterThenTwo.map(function (num) {
-  return num * 3;
-});
-console.log(multByThree);
+// const multByThree = greaterThenTwo.map(function (num) {
+//   return num * 3;
+// });
+// console.log(multByThree);
 
 // const sorted = multByThree.sort((a, b) => a - b);
 // console.log(sorted);
@@ -231,3 +231,106 @@ console.log(multByThree);
 //   .map((num) => num * 3)
 //   .sort((a, b) => a - b);
 // console.log(sorted);
+
+// const players = [
+//   {
+//     id: "player-1",
+//     name: "Mango",
+//     timePlayed: 310,
+//     points: 54,
+//     online: false,
+//     rank: 800,
+//   },
+//   {
+//     id: "player-2",
+//     name: "Poly",
+//     timePlayed: 410,
+//     points: 54,
+//     online: true,
+//     rank: 500,
+//   },
+//   {
+//     id: "player-3",
+//     name: "Kiwi",
+//     timePlayed: 230,
+//     points: 54,
+//     online: true,
+//     rank: 100,
+//   },
+//   {
+//     id: "player-4",
+//     name: "Ajax",
+//     timePlayed: 150,
+//     points: 54,
+//     online: false,
+//     rank: 80,
+//   },
+//   {
+//     id: "player-5",
+//     name: "Chelsy",
+//     timePlayed: 80,
+//     points: 54,
+//     online: true,
+//     rank: 400,
+//   },
+// ];
+
+// const onlineAndSorted = players
+//   .filter((player) => player.online)
+//   .sort((a, b) => a.rank - b.rank);
+// console.table(onlineAndSorted);
+
+// const element = {
+//   class: "",
+//   hovered: false,
+//   changeClass(cls) {
+//     this.class = cls;
+
+//     return this;
+//   },
+//   toggleHovered() {
+//     this.hovered = !this.hovered;
+
+//     return this;
+//   },
+// };
+
+// element.toggleHovered().changeClass("open");
+// console.log(element);
+
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+// console.log(array.flat(3));
+// const tags = tweets.map((t) => t.tags).flat();
+// const tags = tweets.flatMap((t) => t.tags);
+// console.log(tags);
+
+// const stats = tags.reduce((acc, tweet) => [...acc, ...tweet.tags], []);
+// console.log(tags);
+// const stats = tags.reduce((acc, tag) => {
+//   return {
+//     ...acc,
+//     [tag]: acc[tag] ? acc[tag] + 1 : 1,
+//   };
+// }, {});
+
+// const tags = tweets.map((t) => t.tags).flat();
+
+const tags = tweets
+  .flatMap((t) => t.tags)
+  .reduce(
+    (acc, tag) => ({
+      ...acc,
+      [tag]: acc[tag] ? acc[tag] + 1 : 1,
+    }),
+    {}
+  );
+console.log(tags);
+
+console.log(_.isEmpty({}));
+console.log(_.isEmpty({ a: 1 }));
