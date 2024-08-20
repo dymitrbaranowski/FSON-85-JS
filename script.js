37b1f546ebe7c40ed537309fb19239637461262b
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-08-20 14:23:41 +0300

    fox1

--- script.js
+++ script.js
@@ -1156,35 +1156,50 @@ const users = [
 // changeSweaterColor("red");
 // console.log(sweater);
 
-const counter = {
-  value: 0,
-  increment() {
-    console.log("increment -> this", this);
-    this.value += 1;
-  },
-  decrement() {
-    console.log("decrement -> this", this);
-    this.value -= 1;
-  },
+// const counter = {
+//   value: 0,
+//   increment() {
+//     console.log("increment -> this", this);
+//     this.value += 1;
+//   },
+//   decrement() {
+//     console.log("decrement -> this", this);
+//     this.value -= 1;
+//   },
+// };
+
+// const decrementBtn = document.querySelector(".js-decrement");
+// const incrementBtn = document.querySelector(".js-increment");
+// const valueEl = document.querySelector(".js-value");
+
+// decrementBtn.addEventListener("click", function () {
+//   console.log("Кликнули на декремент");
+
+//   counter.decrement();
+//   console.log(counter);
+
+//   valueEl.textContent = counter.value;
+// });
+// incrementBtn.addEventListener("click", function () {
+//   console.log("Кликнули на инкремент");
+
+//   counter.increment();
+//   console.log(counter);
+
+//   valueEl.textContent = counter.value;
+// });
+
+("use strict");
+
+//  console.log(this);
+
+function foo() {
+  console.log(this);
+}
+foo();
+
+const arrow = () => {
+  console.log(this);
 };
 
-const decrementBtn = document.querySelector(".js-decrement");
-const incrementBtn = document.querySelector(".js-increment");
-const valueEl = document.querySelector(".js-value");
-
-decrementBtn.addEventListener("click", function () {
-  console.log("Кликнули на декремент");
-
-  counter.decrement();
-  console.log(counter);
-
-  valueEl.textContent = counter.value;
-});
-incrementBtn.addEventListener("click", function () {
-  console.log("Кликнули на инкремент");
-
-  counter.increment();
-  console.log(counter);
-
-  valueEl.textContent = counter.value;
-});
+arrow();
